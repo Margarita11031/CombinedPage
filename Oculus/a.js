e55c9ca3208c3e5b8603c06cef01210a7f2ead6a
Margarita <margaritasadov@gmail.com>
2016-07-17 23:41:24 +0300

    Initial commit

---
+++ Oculus/a.js
@@ -0,0 +1,9 @@
+/* URL: https://www.oculus.com/en-us/ */
+/* Extended Data: currency_code = {currency_code}, order_id = {order_id}, page_name = {page_name}, qty = {qty}, revenue = {revenue} */
+/* Actions Fired:
+* Id = 11002203421051, Name = Homepage
+*/
+/* Third-Party Tags Fired:
+None
+*/
+!function(){function e(e){function t(e){for(var a=e.childNodes,o=0;o<a.length;o++)t(a[o]);if(e.tagName===n){var c=document.createElement(n);e.getAttribute(r)&&c.setAttribute(r,e.getAttribute(r)),c.text=e.text,e.parentNode.insertBefore(c,e),e.parentNode.removeChild(e)}}var n="SCRIPT",r="src",a=document.createDocumentFragment(),o=document.createElement("I");a.appendChild(o),o.innerHTML='<em style="display:none;">&nbsp;</em>'+e,t(o);var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(a,c)}var t="";if(t){var n=document.write;document.write=function(t){try{n(t)}catch(r){e(t)}},e(t)}}();
